bf647f6c4cb548c59ef8f4d4f2b5a446d1f3220e
Jose Neto <jbqneto@gmail.com>
2025-06-07 07:06:03 +0100

    project start

---
+++ src/main/java/com/jbqneto/dev/botdev/BotdevApplication.java
@@ -0,0 +1,13 @@
+package com.jbqneto.dev.botdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotdevApplication.class, args);
+	}
+
+}
